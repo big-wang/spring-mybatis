6adf910bbcd3cf5dd09e791d3ed1111310d4bc05
wangbr <biren_wang@163.com>
2017-09-23 12:22:54 +0800

    忽略

--- src/main/java/com/wangbr/controller/Hello.java
+++ src/main/java/com/wangbr/controller/Hello.java
@@ -1,16 +1,12 @@
 package com.wangbr.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.wangbr.Model.HelloModel;
-import com.wangbr.entity.CertEntity;
 import com.wangbr.mapper.ICertRepository;
 import com.wangbr.service.TransactionTestService;
 
